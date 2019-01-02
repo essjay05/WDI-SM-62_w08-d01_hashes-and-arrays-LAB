5b6eb4b9cd155f6dd83a64b592ad597b4c0f8091
Joy <stepjoyous@gmail.com>
2019-01-02 15:44:55 -0800

    completed step 2 of arrays and hashes lab

---
+++ arrays-and-hashes-lab.rb
@@ -0,0 +1,16 @@
+pets = [
+    {
+        name: "Steve",
+        type: "dog",
+        age: 5
+    },
+    {
+        name: "Calvin",
+        type: "snake",
+        age: 2
+    }
+]
+# print "My #{type}'s name is #{name}."
+
+p "My #{pets[1][:type]}'s name is #{pets[1][:name]}."
+
