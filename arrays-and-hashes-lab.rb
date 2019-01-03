cd25705e916f72dff04563b2136a40efa9f255dc
Joy <stepjoyous@gmail.com>
2019-01-02 16:22:24 -0800

    completed exercise 2 of arrays and hashes lab

--- arrays-and-hashes-lab.rb
+++ arrays-and-hashes-lab.rb
@@ -23,13 +23,24 @@ p "My #{pets[1][:type]}'s name is #{pets[1][:name]}."
 
 
 #---------- EXERCISE 1 ----------#
-fruits = ["apple", "banana", "orange", "strawberry", "mango"]
-puts fruits
-fruits.map! do |f|
-    if f.include?("b")
-        f.delete("banana")
-    else
-        f
-    end
-end
-puts fruits
+# COMPLETED :)
+# fruits = ["apple", "banana", "orange", "strawberry", "mango"]
+# puts fruits
+# fruits.map! do |f|
+#     if f.include?("b")
+#         f.delete("banana")
+#     else
+#         f
+#     end
+# end
+# puts fruits
+
+#---------- EXERCISE 2 ----------#
+# COMPLETED :)
+# colors = ["blue", "red", "yellow", "black", "white"]
+# puts colors.last
+# puts colors[4]
+# puts colors[colors.length-1]
+# puts colors[colors.length-2]
+
+#---------- EXERCISE 3 ----------#
